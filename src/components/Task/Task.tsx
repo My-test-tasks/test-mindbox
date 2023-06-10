3e946fb041d0ed4ba73296b9cda751845c24daa7
Bumble <bumble.sakh@gmail.com>
2023-06-10 17:49:50 +1100

    feat: add Task component

---
+++ src/components/Task/Task.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react';
+import { Typography, Stack, Checkbox, IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { ITask, todoDelete, todoToggle } from '../../store/todos/slice';
+import { useAppDispatch } from '../../store/hooks';
+
+interface TaskProps {
+  task: ITask;
+}
+
+const Task: FC<TaskProps> = ({ task }) => {
+  const id = task.id;
+
+  const dispatch = useAppDispatch();
+
+  const styles = task.completed ? { textDecoration: 'line-through' } : { textDecoration: 'none' };
+
+  const onChangeHandler = () => {
+    dispatch(todoToggle({ id }));
+  };
+
+  const deleteHandler = () => {
+    dispatch(todoDelete({ id }));
+  };
+
+  return (
+    <Stack direction='row' alignItems='center' spacing={2} sx={styles}>
+      <Checkbox onChange={onChangeHandler} checked={task.completed} />
+      <Typography sx={{ width: '100%' }}>{task.text}</Typography>
+      <IconButton onClick={deleteHandler}>
+        <DeleteIcon />
+      </IconButton>
+    </Stack>
+  );
+};
+
+export default Task;
